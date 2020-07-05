0db8438fea081360b9f0a9da575f0b7cbe5990b8
dipu-725462 <dipu.saha@practo.com>
2020-07-05 23:56:24 +0530

    removed unused code app.js

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View , Button , Alert } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>First React Native APP !</Text>
-      <Button
-          title="Press Me"
-          onPress={() => Alert.alert('Learning React native')}
-        />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text : {
-    marginBottom: 20
-  }
-});
+const styles = StyleSheet.create({});
